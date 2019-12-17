0df0c8ba00defc7d4fe2ac2a73a620769435efa5
matthiasunt <17273057+matthiasunt@users.noreply.github.com>
2019-12-17 21:11:40 +0100

    Implement function

---
+++ src/GoogleImageSearch.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+export class GoogleImageSearch {
+
+  private apiKey: string;
+  private searchEngineId: string;
+
+  constructor(apiKey: string, searchEngineId: string) {
+    this.apiKey = apiKey;
+    this.searchEngineId = searchEngineId;
+  }
+
+  public async getImageUrl(searchString: string) {
+    try {
+      const res: any = await axios.get(`https://www.googleapis.com/customsearch/v1?key=${this.apiKey}&q=
+    ${searchString}&cx=${this.searchEngineId}&searchType=image&enableImageSearch=true`);
+
+      if (res.data.items && res.data.items.length > 0) {
+        return res.data.items[0].link;
+      } else {
+        console.error(res);
+        return "No image found";
+      }
+    } catch (err) {
+      return err;
+    }
+  }
+}
