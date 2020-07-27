7cc2b5edf89c6e79d19ef32ea2d88c0d2051ab05
Matthias Unterfrauner <mattu@post.com>
2020-07-27 17:21:08 +0200

    Refactoring & update Readme

--- src/GoogleImageSearch.ts
+++ src/GoogleImageSearch.ts
@@ -1,9 +1,8 @@
-import axios from "axios";
+import axios from 'axios';
 
 export class GoogleImageSearch {
-
-  private apiKey: string;
-  private searchEngineId: string;
+  private readonly apiKey: string;
+  private readonly searchEngineId: string;
 
   constructor(apiKey: string, searchEngineId: string) {
     this.apiKey = apiKey;
@@ -12,13 +11,18 @@ export class GoogleImageSearch {
 
   public async getImageUrl(searchString: string) {
     try {
-      const res: any = await axios.get(`https://www.googleapis.com/customsearch/v1?key=${this.apiKey}&q=
+      const res: {
+        status: number;
+        data: {
+          items: { link: string; title: string }[];
+        };
+      } = await axios.get(`https://www.googleapis.com/customsearch/v1?key=${this.apiKey}&q=
     ${searchString}&cx=${this.searchEngineId}&searchType=image&enableImageSearch=true`);
 
       if (res.data.items && res.data.items.length > 0) {
         return res.data.items[0].link;
       } else {
-        return "No image found";
+        return 'No image found.';
       }
     } catch (err) {
       return err;
